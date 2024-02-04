6855cd167f19bf03e66f75c9f4f29bcc7e91aace
Alexandr <alexandr.dyomin@yahoo.com>
2024-02-05 01:05:29 +0900

    refactoring

--- routes.js
+++ routes.js
@@ -1,9 +1,7 @@
 const pug = require('pug');
 const { readFileSync } = require('fs');
-const calcBOLL = require('./api/calcBOLL.js');
-const getBarsInfo = require('./api/getBarsInfo.js');
-const analizePrice = require('./api/analizePrice.js');
-const { URL_MARKET_INFO, URL_BARS_INFO, URL_CHART } = require('./api/urls.js');
+const start = require('./api/start.js');
+const { URL_MARKET_INFO } = require('./api/urls.js');
 const getInfoAboutCryptocurrencyPairs = require('./api/getInfoAboutCryptocurrencyPairs.js');
 
 let routes = {
@@ -31,65 +29,17 @@ async function sendIndexPage(req, res) {
         let url = new URL(`${req.headers.host}${req.url}`);
         let interval = url.searchParams.get('interval') || '1d' ;
         let validIntervals = ['1d', '1w', '1M'];
+
         if (!validIntervals.includes(interval)) {
             throw Error('Указан недопустимый интервал. Укажите любой из следующих интервалов: 1d, 1w, 1M');
         }
+
         let data = start(topPairsInfo, interval);
-        let info = await Promise.all(await data);
+        let info = await Promise.all(data);
         info = info.filter((item) => !!item);
         
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         res.end(pug.renderFile('./index.pug', { info, interval, cache: true }));
-
-        async function start(pairsInfo, interval) {
-            let store = [];
-
-            for (let pair of pairsInfo) {
-                let barsInfo =  getBarsInfo({ 
-                    url: URL_BARS_INFO, 
-                    symbol: pair.symbol, 
-                    interval, 
-                    limit: 23 
-                });
-
-                store.push(processKlinesData(barsInfo, pair));
-            }
-
-            return store;
-            
-            async function processKlinesData(data, { symbol,  quoteVolume }) {
-                let res = await data;
-                if (res.length < 23) return null;
-                
-                let beforePenultimateBOLL = calcBOLL(res.slice(0, -2));
-                let penultimateBOLL = calcBOLL(res.slice(1, -1));
-                let currentBOLL = calcBOLL(res.slice(2));
-
-                let summary = analizePrice({ 
-                    beforePenultimatePrice: res.at(-3)[4],
-                    penultimatePrice: res.at(-2)[4],
-                    currentPrice: res.at(-1)[4],
-                    beforePenultimateBOLL, 
-                    penultimateBOLL, 
-                    currentBOLL
-                });
-
-                let result = { 
-                    ...summary, 
-                    symbol, 
-                    currentPrice: formatNumber(+res.at(-1)[4], { digits: 2, prefix: '$' }), 
-                    quoteVolume: formatNumber(+quoteVolume / 1000000, { digits: 2, prefix: '$', postfix: ' млн' }),
-                    chart: `${URL_CHART}/${symbol.slice(0,-4)}_USDT?type=spot`,
-                    diviation: formatNumber(summary.diviation * 100, { digits: 2, postfix: '%' })
-                };
-                
-                return result;
-            }
-
-            function formatNumber(value, { digits, prefix = '', postfix=''}) {
-                return `${prefix}${digits ? value.toFixed(digits) : value}${postfix}`;
-            }
-        }      
     } catch (err) {
         handleError(res, err);
     }
--- urls.js
+++
@@ -1,5 +0,0 @@
-const URL_BARS_INFO = 'https://api.binance.com/api/v1/klines';
-const URL_MARKET_INFO = 'https://api.binance.com/api/v1/ticker/24hr';
-const URL_CHART = 'https://www.binance.com/ru/trade';
-
-export { URL_BARS_INFO, URL_MARKET_INFO, URL_CHART};
